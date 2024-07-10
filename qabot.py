1c1790ff809f227cd5b685900d28ec7663bf7673
Mayur Nikam <mayurnikam266@gmail.com>
2024-07-10 21:16:28 +0530

    changes added

---
+++ qabot.py
@@ -0,0 +1,39 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import streamlit as st
+import os
+import google.generativeai as genai
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+## function to load gemini por model and get response
+model=genai.GenerativeModel("gemini-pro")
+chat=model.start_chat(history=[])
+
+def get_gemini_response(question):
+    response=chat.send_message(question,stream=True)
+    return response
+## initialize our app
+
+st.header("Gemini LLM application")
+
+## initialize session state for chat history if it dosent exist
+
+if 'chat_history' not in st.session_state:
+    st.session_state['chat_history'] =[]
+
+input=st.text_input("input",key="input")
+submit=st.button("Ask the question")
+
+if submit and input:
+    response=get_gemini_response(input)
+
+    ## add user query and response to session cht histoy
+    st.session_state['chat_history'].append(("You",input))
+    st.subheader("The Response is ")
+    for chunk in response:
+        st.write(chunk.text)
+        st.session_state['chat_history'].append(("Bot",chunk.text))
+st.subheader("The chat history is..")
+for role,text in st.session_state['chat_history']:
+    st.write(f"{role}:{text}")
